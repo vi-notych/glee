df994bb3e0fb45a8f3cbec48ad2c21c027996e94
vi-notych <vin.arnet@gmail.com>
2024-07-13 19:38:36 +0300

    add products, lamp, newdesign and video
    секцию  products сделал на гридах
    секцию newdesign на флексах
    MixItUp подключил мимо сборки
    видео тоже открвыается криво, сразу в YouTube

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,10 @@ $(function () {
     //autoplay: true,
     //autoplaySpead: 2000
   })
+
+  var mixer = mixitup('.products__items');
+  var mixer = mixitup('.newdesigne__items');
+
 })
+
+// , .products__items
