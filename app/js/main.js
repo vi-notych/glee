61885c0dfbe0c620e4ddf0dcb022384adeac1804
vi-notych <vin.arnet@gmail.com>
2024-07-15 14:13:48 +0300

    fix 4 sections
    переделал картинки на флексы
    добавил цвета-переменные
    исправил видео, добавил ховер-эфект
    диваны почему-то png с непрозрачным фоном, ховер-эфект работает только на креслах

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,17 @@ $(function () {
     //autoplaySpead: 2000
   })
 
-  var mixer = mixitup('.products__items');
-  var mixer = mixitup('.newdesigne__items');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 })
 
-// , .products__items
